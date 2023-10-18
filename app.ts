cd3ef9c4a737af4400a25f58fc23333df26a52a5
Kevin Thompson <88909437+kevint98@users.noreply.github.com>
2023-10-18 12:20:03 +0100

    Update mongodb url path

--- app.ts
+++ app.ts
@@ -43,7 +43,9 @@ class App {
   private initialiseDatabaseConnection(): void {
     const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
 
-    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+    mongoose.connect(
+      `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`
+    );
   }
 
   public listen(): void {
